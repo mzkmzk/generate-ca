6baf43635a2059ea257720239f411303034ac60c
maizhikun <maizhikun@xunlei.com>
2020-05-06 09:08:47 +0800

    feat add tongji

---
+++ front/plugins/baidu-tongji.js
@@ -0,0 +1,9 @@
+export default function () {
+  var _hmt = _hmt || [];
+  (function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ed3be1e480b77014a42a4b1355da8ad0";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})()
+}
